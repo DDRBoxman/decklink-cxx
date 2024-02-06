8d5389a072703752e425fa8ff8c588d49e484603
Colin Edwards <colin@recursivepenguin.com>
2024-02-05 22:48:45 -0600

    Super jank but a frame shows up

--- include/bridge.h
+++ include/bridge.h
@@ -14,3 +14,40 @@ rust::String GetDisplayName(IDeckLink * deckLink) {
 		returnString = stringBuffer;
 	return returnString;
 }
+
+HRESULT GetOutput(IDeckLink * deckLink, IDeckLinkOutput** deckLinkOutput) {
+    return deckLink->QueryInterface(IID_IDeckLinkOutput, (void**)deckLinkOutput);
+}
+
+const uint32_t kFrameDuration = 1000;
+const uint32_t kTimeScale = 25000;
+const uint32_t kFrameWidth = 1920;
+const uint32_t kFrameHeight = 1080;
+
+// 10-bit YUV row bytes, ref. SDK Manual "2.7.4 Pixel Formats" / bmdFormat10BitYUV
+const uint32_t kRowBytes = ((kFrameWidth + 47) / 48) * 128;
+
+
+const uint32_t kBlueData[] = { 0x40aa298, 0x2a8a62a8, 0x298aa040, 0x2a8102a8 };
+
+static void FillBlue(IDeckLinkMutableVideoFrame* theFrame)
+{
+	uint32_t* nextWord;
+	uint32_t  wordsRemaining;
+	
+	theFrame->GetBytes((void**)&nextWord);
+	wordsRemaining = (kRowBytes * kFrameHeight) / 4;
+	
+	while (wordsRemaining > 0)
+	{
+		*(nextWord++) = kBlueData[0];
+		*(nextWord++) = kBlueData[1];
+		*(nextWord++) = kBlueData[2];
+		*(nextWord++) = kBlueData[3];
+		wordsRemaining = wordsRemaining - 4;
+	}
+}
+
+void Release(IUnknown *obj) {
+    obj->Release();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
-use core_foundation::string::CFStringRef;
-use cxx::UniquePtr;
-use std::pin::Pin;
-use std::ptr::null;
-use std::{pin, ptr::null_mut};
-
-use crate::decklink_ffi::{GetDisplayName, IDeckLink, IDeckLinkIterator};
+use std::{pin::Pin, process::Output};
+use crate::decklink_ffi::{FillBlue, GetDisplayName, GetOutput, IDeckLink, IDeckLinkIterator};
 
 #[cxx::bridge]
 mod decklink_ffi {
@@ -34,17 +29,31 @@ mod decklink_ffi {
 
         unsafe fn GetInt(self: Pin<&mut IDeckLinkAPIInformation>, id: u32, value: *mut i64) -> i32;
 
-        //unsafe fn GetModelName(self: Pin<&mut IDeckLink>, modelName: *const *const c_char) -> i32;
+        type IDeckLinkOutput;
+        type IDeckLinkMutableVideoFrame;
+        type IDeckLinkVideoFrame;
+
+        fn EnableVideoOutput(self: Pin<&mut IDeckLinkOutput>, displayMode: u32, outputFlags: u32) -> i32;
+        fn StartScheduledPlayback(self: Pin<&mut IDeckLinkOutput>, playbackStartTime: i64, timeScale: i64, playbackSpeed: f64) -> i32;
+        unsafe fn ScheduleVideoFrame(self: Pin<&mut IDeckLinkOutput>, frame: *mut IDeckLinkVideoFrame, displayTime: i64, displayDuration: i64, timeScale: i64) -> i32;
+        unsafe fn CreateVideoFrame(self: Pin<&mut IDeckLinkOutput>, width: i32, height: i32, row_bytes: i32, pixel_format: u32, flags: u32, frame: *mut *mut IDeckLinkMutableVideoFrame) -> i32;
+
+        type IUnknown;
 
         include!("decklink-cxx/include/bridge.h");
 
         unsafe fn GetDisplayName(decklink: *mut IDeckLink) -> String;
+
+        unsafe fn GetOutput(decklink: *mut IDeckLink, output: *mut *mut IDeckLinkOutput) -> i32;
+
+        unsafe fn FillBlue(frame: *mut IDeckLinkMutableVideoFrame);
+
+        unsafe fn Release(obj: *mut IUnknown);
     }
 }
 
 fn main() {
     let api_info = decklink_ffi::CreateDeckLinkAPIInformationInstance();
-    println!("{:?}", api_info);
     unsafe {
         let pin: Pin<&mut decklink_ffi::IDeckLinkAPIInformation> =
             Pin::new_unchecked(api_info.as_mut().unwrap());
@@ -55,8 +64,6 @@ fn main() {
     }
 
     let iterator: *mut IDeckLinkIterator = decklink_ffi::CreateDeckLinkIteratorInstance();
-
-    println!("{:?}", iterator);
 
     unsafe {
         let pin: Pin<&mut IDeckLinkIterator> = Pin::new_unchecked(iterator.as_mut().unwrap());
@@ -73,16 +80,53 @@ fn main() {
 
         let name = GetDisplayName(device);
         println!("{}", name);
-        //let pin: Pin<&mut IDeckLink> = Pin::new_unchecked(device.as_mut().unwrap());
 
-        /* pin.GetModelName();
+        let mut output: *mut decklink_ffi::IDeckLinkOutput = std::ptr::null_mut();
+        let output_ptr: *mut *mut decklink_ffi::IDeckLinkOutput = &mut output;
+        let result = GetOutput(device, output_ptr);
+        println!("{}", result);
+        println!("{:p}", output);
 
-        result = deckLink->GetModelName(&deviceNameString);
-        if (result == S_OK)
-        {
-            std::string deviceName = DlToStdString(deviceNameString);
-            printf("=============== %s ===============\n\n", deviceName.c_str());
-            DeleteString(deviceNameString);
-        }*/
+        let mut pin: Pin<&mut decklink_ffi::IDeckLinkOutput> = Pin::new_unchecked(output.as_mut().unwrap());
+        pin.as_mut().EnableVideoOutput(0x48703630, 0);
+        //println!("{}", output.);
+
+        let mut i = 0;
+
+        for i in 0..20 {
+            let mut frame: *mut decklink_ffi::IDeckLinkMutableVideoFrame = std::ptr::null_mut();
+            let frame_ptr: *mut *mut decklink_ffi::IDeckLinkMutableVideoFrame = &mut frame;
+            let result = pin.as_mut().CreateVideoFrame(1920, 1080, ((1920 + 47) / 48) * 128, 0x76323130, 0, frame_ptr);
+            //println!("{}", result);
+            //println!("{:p}", frame);
+
+            FillBlue(frame);
+
+            let result = pin.as_mut().ScheduleVideoFrame(frame as *mut decklink_ffi::IDeckLinkVideoFrame, i*1000, 1000, 25000);
+            println!("schedule {}", result);
+
+            decklink_ffi::Release(frame as *mut decklink_ffi::IUnknown);
+        }
+
+        let result = pin.as_mut().StartScheduledPlayback(0, 25000, 0.2);
+        println!("{}", result);
+
     }
 }
+
+
+/*
+const uint32_t kFrameDuration = 1000;
+const uint32_t kTimeScale = 25000;
+const uint32_t kFrameWidth = 1920;
+const uint32_t kFrameHeight = 1080;
+
+// 10-bit YUV row bytes, ref. SDK Manual "2.7.4 Pixel Formats" / bmdFormat10BitYUV
+const uint32_t kRowBytes = ((kFrameWidth + 47) / 48) * 128;
+
+// 10-bit YUV colour pixels
+const uint32_t kBlueData[] = { 0x40aa298, 0x2a8a62a8, 0x298aa040, 0x2a8102a8 };
+
+    bmdFormat10BitYUV                                            = /* 'v210' */ 0x76323130,
+
+*/
